8adec8189fdfb0806da6857a2cf1f3b0184b98ef
adbroot <64071910+adbroot@users.noreply.github.com>
2021-07-20 17:07:20 +0530

    Add files via upload

---
+++ AWSFoundation-v130-i18-IAMPassword-Policy-MinimumLength/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "remote" {
+    organization = "suhas_TP"
+
+    workspaces {
+      name = "password_policy_minimum_length"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 14
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+}
